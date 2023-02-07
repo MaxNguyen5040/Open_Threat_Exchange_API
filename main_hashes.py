d88220d0ab40f19cfb771f300bd063dff12c1058
MaxNguyen5040 <63136073+MaxNguyen5040@users.noreply.github.com>
2023-02-06 18:35:53 -0600

    Added comments on how program runs

--- main_hashes.py
+++ main_hashes.py
@@ -22,7 +22,7 @@ def main():
     hashes_list = convert_txt_to_list('/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/input/hashes.txt')
     print(hashes_list)
 
-    #if want code to write json files
+    #If we want our code to write our JSON files locally
     write_to_files = False
 
     error_hashes = []
@@ -35,33 +35,39 @@ def main():
     #     if analysis_response:
     #         error_hashes.append(analysis_response)
 
+    #If there are error values, usually API issues like the servers are down, print them here and wait for manual command to move on. 
     if error_hashes != []:
         print("Error hashes:",error_hashes)
         input('move on?')
-        #ADD CODE IN FOR WHAT TO DO AFTER WRITE TO CSV - LIKE REDO THESE HASHES
 
-    #add both sets of data to csv
+    #Add both sets of data to csv
     csv_writer('/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/response_output/hash/general','/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/response_output/hash/analysis',
                '/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/results/hashes/hashes_detections.csv','/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/results/hashes/hashes_general.csv')
 
 def csv_writer(general_folder,analysis_folder,detections_file_name,general_file_name):
 
+    #Creating the output CSV file, found in "Results" and "Hashes" folders. 
     detections_json_files = [pos_json for pos_json in os.listdir(general_folder)]
     detections_fields = ['number', 'hash_id','report_id','hash', 'pulses', 'sha1', 'ssdeep', 'sha256', 'md5',
               'adobemalwareclassifier', 'apk', 'avast', 'avg', 'clamav', 'cuckoo', 'disa_entrypoint', 'exiftool', 'machoinfo',
               'msdefender','metaextract', 'pe32info', 'peanomal', 'ratdecoder', 'strings', 'yarad', 'zipfiles']
 
     count = 0
+    
+    #Opening the result CSV file
     with open(detections_file_name, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(detections_fields)
 
+        #Saving the basic data of the hash input, such as the hash value, encoding types, etc. 
         for i in detections_json_files[0:30]:
             row = [count]
             z = i.split("_")
             row += z
             row.pop(-1)
-
+            
+            #Accessing previously created file that utilizes "general" API call to save pulse data to the spreadsheet
+            #Pulses are individual "communities" on the Open Threat Exchange that watch for any activity of any given hash value, noting down malicious behavior. 
             with open('/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/response_output/hash/general/' + i) as file:
                 data = json.load(file)
                 row.append(data['indicator'])
@@ -69,14 +75,18 @@ def csv_writer(general_folder,analysis_folder,detections_file_name,general_file_
 
 
             try:
+                #Opens previously created file that utilizes "analysis" API call to save more in depth data in CSV file
                 with open('/Users/maxnguyen/PycharmProjects/OTX_Api_Test/data/response_output/hash/analysis/' + i) as file2:
                     data = json.load(file2)
                     if data["page_type"] != "generic":
+                        
+                        #these are different encoding methods that represent the same hash value. 
                         row.append(data['analysis']['info']['results']['sha1'])
                         row.append(data['analysis']['info']['results']['ssdeep'])
                         row.append(data['analysis']['info']['results']['sha256'])
                         row.append(data['analysis']['info']['results']['md5'])
 
+                        #Analysis offers many Malware detection program's output when given this given hash value, including Microsoft Defender, or Strings. 
                         plugins_names = [k for k, v in data['analysis']['plugins'].items()]
                         print(plugins_names)
                         for w in detections_fields[9:]:
@@ -107,7 +117,7 @@ def csv_writer(general_folder,analysis_folder,detections_file_name,general_file_
     general_fields = ['number','hash_id','report_id','hash','pulses','earliest_pulse_date','recentmost_pulse_date','plugin_amount','analysis_date']
 
     count = 0
-
+    #Creating a more in depth CSV file containing all data
     with open(general_file_name, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(general_fields)
@@ -122,7 +132,8 @@ def csv_writer(general_folder,analysis_folder,detections_file_name,general_file_
                 data = json.load(file)
                 row.append(data['indicator'])
                 row.append(data['pulse_info']['count'])
-
+                
+                #Saving most recent and most long ago date and time information
                 if data['pulse_info']['count'] > 0:
                     smallest_date = data['pulse_info']['pulses'][0]['created'][:10]
                     formatted_smallest_date = time.strptime(smallest_date, "%Y-%m-%d")
@@ -165,7 +176,7 @@ def csv_writer(general_folder,analysis_folder,detections_file_name,general_file_
 
 
 
-
+#converts given txt file containg Hash values into Python list format. 
 def convert_txt_to_list(filename):
     list_of_hashes_and_values = []
 
@@ -182,6 +193,7 @@ def convert_txt_to_list(filename):
 
     return list_of_hashes_and_values
 
+#method to call API
 def otx_hash_api_response(hash_id, report_id, hash,write_files,type):
     #setup
     url = 'https://otx.alienvault.com/api/v1/indicators/file/' + hash + '/'+type
